de92a8686c03e75c2d6522e1884ea0347c36cd5f
jobdev-hub <job.methods@outlook.com>
2021-11-10 20:48:48 -0300

    2 files modified

--- src/app/table-dynamic-columns-example.ts
+++ src/app/table-dynamic-columns-example.ts
@@ -1,11 +1,5 @@
 import { Component } from '@angular/core';
-const RESPONSE_API: any[] = [
-  { id: 1, info1: 'A', info2: 1.1, info3: 'A', info4: '1' },
-  { id: 2, info1: 'B', info2: 2.2, info3: 'B', info4: null },
-  { id: 3, info1: 'C', info2: 3.3, info3: 'C', info4: '-' },
-  { id: 4, info1: 'D', info2: 4.4, info3: 'D', info4: '+' },
-  { id: 5, info1: 'E', info2: 5.5, info3: 'E', info4: '/' },
-];
+
 @Component({
   selector: 'table-dynamic-columns-example',
   styleUrls: ['table-dynamic-columns-example.css'],
@@ -13,13 +7,31 @@ const RESPONSE_API: any[] = [
 })
 export class TableDynamicColumnsExample {
   displayedColumns: string[] = [];
-  data: any[] = RESPONSE_API;
+  data: any[] = [];
+  tabelaVisivel = false;
+
   ngOnInit() {
-    this.definirColunas();
   }
+
+  charge() {
+    this.data = [
+      { id: 1, info1: 'A', info2: 1.1, info3: 'A', info4: '1' },
+      { id: 2, info1: 'B', info2: 2.2, info3: 'B', info4: null },
+      { id: 3, info1: 'C', info2: 3.3, info3: 'C', info4: '-' },
+      { id: 4, info1: 'D', info2: 4.4, info3: 'D', info4: '+' },
+      { id: 5, info1: 'E', info2: 5.5, info3: 'E', info4: '/' },
+    ];
+  }
+
   definirColunas() {
-    RESPONSE_API.length > 0
-      ? (this.displayedColumns = Object.keys(RESPONSE_API[0]))
+    this.data.length > 0
+      ? (this.displayedColumns = Object.keys(this.data[0]))
       : (this.displayedColumns = ['nenhum registro encontrado!']);
+  }
+
+  montarTabela(){
+    this.tabelaVisivel = true;
+    this.charge();
+    this.definirColunas();
   }
 }
